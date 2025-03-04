39fe503dc9a21bfddc06a7bca42bfdd0731121ce
Proverbs35 <ncancel@fingerlakes.edu>
2025-03-04 17:54:33 -0500

    Update triangle_area.py

--- triangle_area.py
+++ triangle_area.py
@@ -17,7 +17,7 @@ height = int(input('Enter the height: '))
 base = int(input('Enter the base: '))
 
 area = (1/2) * height * base
-
+print()
 print('The area of the triangle is ', area)  
 print()
 print("'''")
